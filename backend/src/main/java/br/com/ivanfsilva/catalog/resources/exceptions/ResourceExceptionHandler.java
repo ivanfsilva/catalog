c865f1d9579e45cb1cab5f977002947851613c77
ivanfsilva <ivanfs27@gmail.com>
2021-05-22 15:14:29 -0300

    Refactor pageable

--- backend/src/main/java/br/com/ivanfsilva/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/ivanfsilva/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package br.com.ivanfsilva.catalog.resources.exceptions;
 
 import java.time.Instant;
 
-import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
