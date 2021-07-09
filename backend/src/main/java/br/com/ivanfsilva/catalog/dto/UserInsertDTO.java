33f336ee989a9a07af74bffe3a649d106f0cde90
ivanfsilva <ivanfs27@gmail.com>
2021-07-09 19:29:16 -0300

    Custom ConstraintValidator

--- backend/src/main/java/br/com/ivanfsilva/catalog/dto/UserInsertDTO.java
+++ backend/src/main/java/br/com/ivanfsilva/catalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package br.com.ivanfsilva.catalog.dto;
 
+import br.com.ivanfsilva.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
 
---
+++ backend/src/main/java/br/com/ivanfsilva/catalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,44 @@
+package br.com.ivanfsilva.catalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.ivanfsilva.catalog.dto.UserInsertDTO;
+import br.com.ivanfsilva.catalog.entities.User;
+import br.com.ivanfsilva.catalog.repositories.UserRepository;
+import br.com.ivanfsilva.catalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        // Coloque aqui os testes de validação, acrescentando objetos FieldMessage à lista
+
+        User user = repository.findByEmail(dto.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
